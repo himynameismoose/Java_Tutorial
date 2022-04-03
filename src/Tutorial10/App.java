8580988dcd832f26dd57ecab07c8e42cc39f59ee
himynameismoose <mershellemae@gmail.com>
2022-04-03 16:07:14 -0700

    Arrays

---
+++ src/Tutorial10/App.java
@@ -0,0 +1,34 @@
+package Tutorial10;
+
+// Arrays
+public class App {
+    public static void main(String[] args) {
+
+        int value = 7;
+
+        int[] values;
+
+        values = new int[3];
+
+        System.out.println(values[0]);
+
+        values[0] = 10;
+        values[1] = 20;
+        values[2] = 30;
+
+        System.out.println(values[0]);
+        System.out.println(values[1]);
+        System.out.println(values[2]);
+
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(values[i]);
+        }
+
+        // initialize array before declaring it
+        int[] numbers = {5, 6, 7};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
