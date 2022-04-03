a83028f5a65aed0098a9c13fb3694096894187ba
himynameismoose <mershellemae@gmail.com>
2022-04-03 15:05:15 -0700

    For loops

---
+++ src/Tutorial5/Application.java
@@ -0,0 +1,10 @@
+package Tutorial5;
+
+public class Application {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) { // i++ -> i = i + 1
+            System.out.printf("The value of i is: %d\n", i);
+        }
+    }
+}
