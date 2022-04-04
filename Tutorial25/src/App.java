d8fec0b2eb2bf332f7a05c5df33c53dc16554503
himynameismoose <mershellemae@gmail.com>
2022-04-04 15:56:35 -0700

    Public, Private, Protected

--- Tutorial25/src/App.java
+++ Tutorial25/src/App.java
@@ -1,5 +1,12 @@
 import world.Plant;
 
+/*
+ * private --- only within same class
+ * public --- from anywhere
+ * protected --- same class, subclass, and same package
+ * no modifier --- same package only
+ */
+
 public class App {
     public static void main(String[] args) {
         Plant plant = new Plant();
