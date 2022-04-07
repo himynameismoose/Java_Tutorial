3bfc4d9a0c42cc87ff51e3e6960ee0f0ad71e6c2
himynameismoose <mershellemae@gmail.com>
2022-04-07 10:50:20 -0700

    creating and writing files

---
+++ Tutorial41/src/App.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+/** Creating and writing files **/
+public class App {
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+
+        try (BufferedWriter br = new BufferedWriter(new FileWriter(file))) {
+            br.write("This is line one");
+            br.newLine();
+            br.write("This is line two");
+            br.newLine();
+            br.write("Last line");
+        }  catch (IOException e) {
+            System.out.println("Unable to read file " + file);
+        }
+    }
+}
