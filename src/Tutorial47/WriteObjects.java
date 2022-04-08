273823b8cf881c617a0e7f40a784b0aa7f4ec6a7
himynameismoose <mershellemae@gmail.com>
2022-04-08 12:21:37 -0700

    serializing arrays

---
+++ src/Tutorial47/WriteObjects.java
@@ -0,0 +1,35 @@
+package Tutorial47;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class WriteObjects {
+    public static void main(String[] args) {
+        System.out.println("Writing objects ...");
+        Person[] people = { new Person(1, "Sue"),
+                            new Person(99, "Mike"),
+                            new Person(7, "Bob")};
+
+        ArrayList<Person> peopleList = new ArrayList<>(Arrays.asList(people));
+
+        try (FileOutputStream fs = new FileOutputStream("test.ser");
+             ObjectOutputStream os = new ObjectOutputStream(fs)) {
+
+            os.writeObject(people);
+
+            os.writeObject(peopleList);
+
+            os.writeInt(peopleList.size());
+
+            for (Person person : peopleList) {
+                os.writeObject(person);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
